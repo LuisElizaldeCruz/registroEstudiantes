519c7b0e519ec80ce6dbc3dd722aff8480248734
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-07-20 23:05:56 -0600

    se agrego los endpoints para listar todas las academias y el endpoint para eliminar una academia

--- src/main/java/com/escuela/studentsData/controllers/AcademiaController.java
+++ src/main/java/com/escuela/studentsData/controllers/AcademiaController.java
@@ -3,8 +3,10 @@ package com.escuela.studentsData.controllers;
 import com.escuela.studentsData.entities.Academia;
 import com.escuela.studentsData.service.AcademiaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,9 +22,13 @@ public class AcademiaController {
         return (Academia) academiaService.findById(id).orElse(null);
     }
     */
+    @GetMapping
+    public List<Academia> listarAcademias() {
+        return academiaService.findAll();
+    }
 
     @GetMapping("/obtener/{id}")
-    public Academia getAcademia(@PathVariable Long id) {
+    public Academia econtrarAcademia(@PathVariable Long id) {
        Optional <Academia> academia = academiaService.findById(id);
 
        if(!academia.isPresent()) {
@@ -32,10 +38,22 @@ public class AcademiaController {
         return academia.get();
     }
 
-
     @PostMapping("/crear")
-    public Academia crear(@RequestBody Academia academia) {
+    public Academia crearAcademia(@RequestBody Academia academia) {
         return academiaService.save(academia);
+    }
+
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminarAcademia(@PathVariable Long id){
+        Optional <Academia> academia = academiaService.findById(id);
+
+        if(!academia.isPresent()) {
+            throw new RuntimeException("Academia no encontrada con id: " + id);
+        }
+        academiaService.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 
 }
